3a1a28037b2f24a4fbf46b0416a8c7423a1c227e
Alexander Efimov <efimokthefatass@gmail.com>
2022-06-23 15:54:11 +0300

    update instructions

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,13 @@
-set -e
+cd "$( dirname "$0" )/.."
+
 # MAIN APP INSTALLATION
 npm i
 npm run build
 
 # MAIN APP AUTOSTART
-echo '/home/pi/solar-energy-ui/dist/linux-armv7l-unpacked/solar-energy-ui' > ~/.xinitrc
-chmod +x ~/.xinitrc
+chmod +x dist/hydrogen-energy-ui*.AppImage
+mkdir -p ~/inenergy-gui/dist
+cp dist/hydrogen-energy-ui*.AppImage ~/inenergy-gui/dist/
+mkdir ~/.inenergy
+echo '~/inengergy-gui/dist/hydrogen-energy-ui*.AppImage > ~/.inenergy/hydrogen-energy-ui.log' > ~/.config/openbox/autostart
+echo '{"CRITICAL_CONCENTRATION": 75}' > ~/.inenergy/config.json
